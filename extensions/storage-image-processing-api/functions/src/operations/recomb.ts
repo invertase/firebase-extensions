002445670fb33211337bff6175105ff5263b839d
dackers86 <ackers86@hotmail.com>
2021-06-25 11:04:35 +0100

    Added recomb operation

---
+++ extensions/storage-image-processing-api/functions/src/operations/recomb.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as superstruct from 'superstruct';
+import * as utils from '../utils';
+import { OperationBuilder } from '../types';
+
+/**
+ * The user visible name of this operation.
+ */
+const name = 'recomb';
+
+/**
+ * Apply the recomb formula a * input + b to the image (levels adjustment)
+ */
+const struct = superstruct.object({
+  operation: superstruct.literal(name),
+
+  /**
+   * Multiplier (optional, default 1.0)
+   */
+  inputMatrix: utils.coerceStringToArray(
+    superstruct.size(
+      superstruct.array(superstruct.array(superstruct.number())),
+      3,
+      3,
+    ),
+  ),
+});
+
+export type OperationRecomb = superstruct.Infer<typeof struct>;
+
+export const operationRecomb: OperationBuilder = {
+  name,
+  struct,
+
+  build(operation) {
+    const options = operation.options as OperationRecomb;
+
+    return [
+      {
+        method: name,
+        arguments: [options.inputMatrix, utils.omitKey(options, 'inputMatrix')],
+      },
+    ];
+  },
+};
