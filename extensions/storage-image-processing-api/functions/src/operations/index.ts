739d78f26d190ad947a31425446939179400a1b7
Darren Ackers <darren@invertase.io>
2021-07-08 11:43:47 +0100

    Added modulate operation (#8)
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- extensions/storage-image-processing-api/functions/src/operations/index.ts
+++ extensions/storage-image-processing-api/functions/src/operations/index.ts
@@ -50,6 +50,7 @@ import { operationAffine } from './affine';
 import { operationClahe } from './clahe';
 import { operationThreshold } from './threshold';
 import { operationConvolve } from './convolve';
+import { operationModulate } from './modulate';
 import { operationLinear } from './linear';
 
 export * from './input';
@@ -74,6 +75,7 @@ export * from './affine';
 export * from './clahe';
 export * from './threshold';
 export * from './convolve';
+export * from './modulate';
 export * from './linear';
 
 const builders: { [key: string]: OperationBuilder } = {
@@ -99,6 +101,7 @@ const builders: { [key: string]: OperationBuilder } = {
   clahe: operationClahe,
   threshold: operationThreshold,
   convolve: operationConvolve,
+  modulate: operationModulate,
   linear: operationLinear,
 };
 
