ecf474fb7f4004ce26a37068236e63e492d645fd
Darren Ackers <darren@invertase.io>
2021-07-08 11:57:52 +0100

    Added grayscale operation (#11)
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- extensions/storage-image-processing-api/functions/src/operations/index.ts
+++ extensions/storage-image-processing-api/functions/src/operations/index.ts
@@ -50,6 +50,7 @@ import { operationAffine } from './affine';
 import { operationClahe } from './clahe';
 import { operationThreshold } from './threshold';
 import { operationConvolve } from './convolve';
+import { operationGrayScale } from './grayscale';
 import { operationModulate } from './modulate';
 import { operationLinear } from './linear';
 
@@ -75,6 +76,7 @@ export * from './affine';
 export * from './clahe';
 export * from './threshold';
 export * from './convolve';
+export * from './grayscale';
 export * from './modulate';
 export * from './linear';
 
@@ -101,6 +103,7 @@ const builders: { [key: string]: OperationBuilder } = {
   clahe: operationClahe,
   threshold: operationThreshold,
   convolve: operationConvolve,
+  grayscale: operationGrayScale,
   modulate: operationModulate,
   linear: operationLinear,
 };
