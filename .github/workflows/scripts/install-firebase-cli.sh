d6a042c8d6fbcab5a01829c8bcb9d0653567e68f
Salakar <mike.diarmid@gmail.com>
2021-07-13 17:10:19 +0100

    -

--- .github/workflows/scripts/install-firebase-cli.sh
+++ .github/workflows/scripts/install-firebase-cli.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# TODO switch to official release once fixes are published.
-git clone --single-branch --branch jh-ext-emu-fixes https://github.com/firebase/firebase-tools.git
-cd firebase-tools
-npm i
-npm link --local
+# Use the below to use a specific branch of firebase-tools
+# git clone --single-branch --branch jh-ext-emu-fixes https://github.com/firebase/firebase-tools.git
+# cd firebase-tools
+# npm i
+# npm link --local
+npm i -g firebase-tools@latest
 firebase --open-sesame extdev
