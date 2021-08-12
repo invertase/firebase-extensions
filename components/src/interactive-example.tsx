f9978d0f7b6b43f43bf605bcc7dffc88f004507a
dackers86 <ackers86@hotmail.com>
2021-08-12 11:04:01 +0100

    Added compiled example

---
+++ components/src/interactive-example.tsx
@@ -0,0 +1,7 @@
+import React, { createElement, HTMLProps, ReactNode } from 'react';
+
+const InteractiveExample = (): JSX.Element => {
+  return <div>Working example</div>;
+};
+
+export default InteractiveExample;
