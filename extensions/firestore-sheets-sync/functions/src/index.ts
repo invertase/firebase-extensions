da3ecf6f9ed066a338067bee10b0e390f4c1238a
Mais Alheraki <mais.alheraki@gmail.com>
2023-02-27 20:36:53 +0300

    Update extensions/firestore-sheets-sync/functions/src/index.ts
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- extensions/firestore-sheets-sync/functions/src/index.ts
+++ extensions/firestore-sheets-sync/functions/src/index.ts
@@ -6,7 +6,6 @@ import config from './config';
 
 const sheets = google.sheets('v4');
 const authClient = google.auth.getClient({
-  // keyFilename: 'extensions-testing-cae073db7903.json',
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
