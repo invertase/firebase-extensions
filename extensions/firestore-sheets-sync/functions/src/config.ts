9c240c860282ad56efaad8f0448907d3e0a0389b
Mais <mais.alheraki@gmail.com>
2023-02-27 15:45:03 +0300

    feat: syncing implementation

--- extensions/firestore-sheets-sync/functions/src/config.ts
+++ extensions/firestore-sheets-sync/functions/src/config.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 export default {
-    collection: process.env.FIRESTORE_COLLECTION
-}
+  collection: process.env.FIRESTORE_COLLECTION,
+  spreadsheetId: process.env.SHEET_ID,
+  fields: process.env.FIELDS_TO_SYNC!,
+};
--- extensions/firestore-sheets-sync/functions/src/index.ts
+++ extensions/firestore-sheets-sync/functions/src/index.ts
@@ -1,6 +1,127 @@
-import * as functions from "firebase-functions";
-import config from "./config";
+import * as functions from 'firebase-functions';
+import { GaxiosError } from 'gaxios';
+import { google, sheets_v4 } from 'googleapis';
 
-export const googleSheetSync = functions.firestore.document(`${config.collection}`).onWrite((change, context) => {
-  console.log("Hello from Google Sheets Sync!");
+import config from './config';
+
+const sheets = google.sheets('v4');
+const authClient = google.auth.getClient({
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
+
+const fields = config.fields.split(',');
+
+export const googleSheetSync = functions.firestore
+  .document(`${config.collection}`)
+  .onWrite(async change => {
+    // Get the document that was written to Firestore, and structure it.
+    const data = change.after.data();
+
+    if (!data) return;
+
+    await createHeaderRow();
+
+    const rows: any[] = [];
+
+    console.log(fields);
+
+    for (const field of fields) {
+      const value = data[field];
+      if (value) rows.push(value);
+      else rows.push('');
+    }
+
+    try {
+      // Append the data to the Google Sheet.
+      await appendNewRow(rows);
+
+      return;
+    } catch (error) {
+      // Log an error if the data was not written to the Google Sheet.
+      functions.logger.error((error as GaxiosError).message, error);
+    }
+  });
+
+async function appendNewRow(data: any[]) {
+  return sheets.spreadsheets.values.append({
+    auth: await authClient,
+    spreadsheetId: config.spreadsheetId,
+    valueInputOption: 'RAW',
+    requestBody: { values: [data], range: 'A:Z' },
+    range: 'A:Z',
+  });
+}
+
+async function createHeaderRow() {
+  const values = fields.map<sheets_v4.Schema$CellData>(e => ({
+    userEnteredValue: { stringValue: e as string },
+    userEnteredFormat: {
+      textFormat: {
+        bold: true,
+      },
+    },
+  }));
+
+  const headerRow = await sheets.spreadsheets.values.get({
+    auth: await authClient,
+    spreadsheetId: config.spreadsheetId,
+    range: 'A1:Z1',
+    majorDimension: 'ROWS',
+  });
+
+  // Check if the header row is already set up.
+  if (
+    headerRow.data.values &&
+    arrayEqual(headerRow.data.values[0].sort(), [...fields].sort())
+  )
+    return;
+
+  sheets.spreadsheets.batchUpdate({
+    auth: await authClient,
+    spreadsheetId: config.spreadsheetId,
+    requestBody: {
+      requests: [
+        {
+          insertDimension: {
+            range: {
+              dimension: 'ROWS',
+              startIndex: 0,
+              endIndex: 1,
+            },
+            inheritFromBefore: false,
+          },
+        },
+        {
+          updateSheetProperties: {
+            properties: {
+              gridProperties: { frozenRowCount: 1 },
+            },
+            fields: 'gridProperties.frozenRowCount',
+          },
+        },
+        {
+          updateCells: {
+            rows: [
+              {
+                values,
+              },
+            ],
+            fields: '*',
+            range: {
+              startRowIndex: 0,
+              endRowIndex: 1,
+            },
+          },
+        },
+      ],
+    },
+  });
+}
+
+function arrayEqual(arr1: any[], arr2: any[]) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+  return (
+    arr1.every(item => set2.has(item)) && arr2.every(item => set1.has(item))
+  );
+}
